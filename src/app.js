fda7ca2e6e70736d5f143ef04da3c11628db71ef
Damião V. Souza <139881408+damiaovieiraa@users.noreply.github.com>
2025-05-30 15:04:46 -0300

    swagger config

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const router = require("./routes");
 const cors = require("cors");
+const swaggerUi = require('swagger-ui-express');
+const swaggerSpec = require("./docs/swagger");
 
 const app = express();
 
 app.use(cors());
 
 router(app, express);
+
+app.use("/", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 module.exports = app;
---
+++ src/docs/swagger.js
@@ -0,0 +1,38 @@
+const swaggerJSDoc = require("swagger-jsdoc");
+
+const swaggerDefinition = {
+    openapi: "3.0.0",
+    info: {
+        title: "Biblioteca Online",
+        version: "1.0.0",
+        description: "Documentação da API com exemplos de requisições e respostas"
+    },
+    components: {
+        securitySchemes: {
+            bearerAuth: {
+                type: 'http',
+                scheme: 'bearer',
+                bearerFormat: 'JWT',
+            }
+        }
+    },
+    security: [
+        {
+            bearerAuth: [],
+        }
+    ],
+    servers: [
+        {
+            url: "https://biblioteca-online-2s7u.onrender.com"
+        }
+    ]
+};
+
+const options = {
+    swaggerDefinition,
+    apis: ["../routes/*.js"]
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+
+module.exports = swaggerSpec;
